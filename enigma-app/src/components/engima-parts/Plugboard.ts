7421e5e0fea730baa003e52579f7a967fbdb1f07
Mark Turnbull <mrktrnbll@gmail.com>
2025-02-10 13:52:43 +0000

    added passthrough switch board funcitonality

--- enigma-app/src/components/engima-parts/Plugboard.ts
+++ enigma-app/src/components/engima-parts/Plugboard.ts
@@ -4,3 +4,15 @@ import {PLUGBOARD_SETTINGS} from "./Variables";
 export const printPlugboardSettings: () => void = () => {
     console.log(PLUGBOARD_SETTINGS);
 }
+
+export const runLetterThroughPlugboard: (letter: string, forwardDirection: boolean) => string = (letter, forwardDirection) => {
+    if (forwardDirection) {
+        return PLUGBOARD_SETTINGS.backward.charAt(PLUGBOARD_SETTINGS.forward.indexOf(letter));
+    } else {
+        return PLUGBOARD_SETTINGS.forward.charAt(PLUGBOARD_SETTINGS.backward.indexOf(letter));
+    }
+}
+
+// TODO: add way to set plugboard settings
+
+console.log(runLetterThroughPlugboard("A", true)) // "tested" and works
