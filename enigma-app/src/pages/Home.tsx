2b67be3767d6733a892949f8c8623f143df12a64
Mark Turnbull <mrktrnbll@gmail.com>
2025-02-10 14:58:54 +0000

    [WIP] - added rotor class and method rotate that handles when the notch should tick the next rotor over

--- enigma-app/src/pages/Home.tsx
+++ enigma-app/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import '../app/globals.css'
 import { useState, useCallback } from "react";
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {Rotor} from "@/components/engima-parts/Rotors";
 
 export default function Home() {
   const [camera, setCamera] = useState(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
@@ -20,6 +21,16 @@ export default function Home() {
       controls.update()
       controls.enabled = true;
   }, [camera, controls]);
+
+  // TODO: Remove this later
+  // Testing the rotor class
+  const rotor_m: Rotor  = new Rotor("1", "A", null, null)
+  const rotor_l: Rotor  = new Rotor("2", "A", rotor_m, null)
+
+  for (let i = 0; i < 30; i++) {
+      console.log(rotor_l.rotate());
+  }
+  console.log(rotor_m.rotate());
 
   return (
       <div style={{position: 'relative', width: '100vw', height: '100vh'}}>
