d30f6aa796f74c51994eafbfd14da22d62baa3f8
Mark Turnbull <mrktrnbll@gmail.com>
2025-02-10 14:58:54 +0000

    [WIP] - added rotor class and method rotate that handles when the notch should tick the next rotor over

--- enigma-app/src/components/engima-parts/Rotors.ts
+++ enigma-app/src/components/engima-parts/Rotors.ts
@@ -1,5 +1,30 @@
-import {ROTOR_PAIRINGS, NOTCHES} from "@/components/engima-parts/Variables";
+import {ROTOR_PAIRINGS, NOTCHES, ALPHABET} from "@/components/engima-parts/Variables";
 
 export const printRotors: () => void = () => {
     console.log(ROTOR_PAIRINGS, NOTCHES);
+}
+
+export class Rotor {
+    rotorID: string;
+    rotorNumber: string;
+    nextRotor: Rotor | null;
+    previousRotor: Rotor | null;
+    changeNextRotor: string;
+
+    constructor(rotorID: string, rotorNumber: string, nextRotor: Rotor | null, previousRotor: Rotor | null) {
+        this.rotorID = rotorID;
+        this.rotorNumber = rotorNumber;
+        this.nextRotor = nextRotor;
+        this.previousRotor = previousRotor;
+        this.changeNextRotor = NOTCHES[rotorID];
+    }
+
+    rotate(): void {
+        if (this.nextRotor && this.rotorNumber==this.changeNextRotor) {
+            console.log("step");
+            this.nextRotor.rotate();
+        }
+        this.rotorNumber = ALPHABET.charAt((ALPHABET.indexOf(this.rotorNumber) + 1) % 26);
+        console.log(this.rotorNumber);
+    }
 }
