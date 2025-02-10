50c7ed2fac6486b7ec334d13b289b575bd9ce51c
Mark Turnbull <mrktrnbll@gmail.com>
2025-02-10 17:35:39 +0000

    refactored way plugboard works to align with reflector - both the same idea but plugboard is changeable

--- enigma-app/src/components/engima-parts/Plugboard.ts
+++ enigma-app/src/components/engima-parts/Plugboard.ts
@@ -1,18 +1,23 @@
-import {PLUGBOARD_SETTINGS} from "./Variables";
+import {PLUGBOARD_SETTINGS, REFLECTOR} from "./Variables";
 
 
 export const printPlugboardSettings: () => void = () => {
     console.log(PLUGBOARD_SETTINGS);
 }
 
-export const runLetterThroughPlugboard: (letter: string, forwardDirection: boolean) => string = (letter, forwardDirection) => {
-    if (forwardDirection) {
-        return PLUGBOARD_SETTINGS.backward.charAt(PLUGBOARD_SETTINGS.forward.indexOf(letter));
+export const runLetterThroughPlugboard: (letter: string) => string = (letter: string) => {
+    if (PLUGBOARD_SETTINGS.forward.includes(letter)) {
+        return REFLECTOR.backward.charAt(REFLECTOR.forward.indexOf(letter));
+    } else if (PLUGBOARD_SETTINGS.backward.includes(letter)) {
+        return REFLECTOR.forward.charAt(REFLECTOR.backward.indexOf(letter));
     } else {
-        return PLUGBOARD_SETTINGS.forward.charAt(PLUGBOARD_SETTINGS.backward.indexOf(letter));
+        return letter;
     }
 }
 
-// TODO: add way to set plugboard settings
+// TODO:
+//  - add way to set plugboard settings
+//  - test runLetterThroughPlugboard(), refactored so works similar as Reflector passthrough funciton
+
 
 console.log(runLetterThroughPlugboard("A", true)) // "tested" and works
