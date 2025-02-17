418797dd2a8b71eee1e66d126f6265a4a12f503a
Mark Turnbull <mrktrnbll@gmail.com>
2025-02-17 13:31:50 +0000

    [WIP] - added output flag

--- enigma-app/next.config.ts
+++ enigma-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   transpilePackages: ['three'],
   eslint: {
     ignoreDuringBuilds: true,
